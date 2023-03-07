0c61dd7d600f268ffe3579cb91039badb01eaac0
Grandereohb <qpyang0108@gmail.com>
2023-03-07 16:35:49 +0800

    添加了终点节点判别，修改了树策略与选取最优子节点的选择方式/模拟与反向传播中运动成本的计算公式

--- src/test/view_planner/include/MCST.h
+++ src/test/view_planner/include/MCST.h
@@ -8,28 +8,32 @@ public:
     State state;                  // 节点的状态
     Action action;                // 指向该节点状态的动作
     TreeNode *parent;             // 父节点
+    TreeNode *best_child;         // 最优子节点
     bool has_child;               // 记录有无已访问的子节点
+    bool is_terminal;             // 记录是否为满足测量要求的终点
     vector<TreeNode> children;    // 当前的子节点
 
     TreeNode(const State &state, TreeNode *parent);
     TreeNode(const State &state);
     TreeNode(const TreeNode* node);
-    // TreeNode(){}
+    TreeNode();
     ~TreeNode(){}
 
     bool initializeNode(const vector<ViewPoint> &candidates);  // 初始化节点
     TreeNode *expand(int id);                                  // 扩展子节点
     int isFullyExpanded();                                     // 检查是否完全扩展
     TreeNode *randomChild();                                   // 选择随机子节点
-    TreeNode *weightedBestChild();                             // 选择加权最佳子节点
+    TreeNode *weightedBestChild(
+        const vector<vector<ViewPoint>>& graph);               // 选择加权最佳子节点
     TreeNode *bestChild();                                     // 选择最佳子节点
     TreeNode *applyAction(const Action &action);               // 执行action
     double getTravelCost(int start, int end, Graph *g);        // 计算运动成本
     void addVisitNum();                                        // 增加节点的num_visits
     void addCost(double once_cost);                            // 增加节点的cost
-    void updateMinCost(double once_cost);                      // 更新min_cost
+    void updateMinCost(double once_cost, TreeNode* pre);       // 更新min_cost与最优子节点
     void setUnreachable(int id);                               // 将子节点设置为无法到达
-    
+    double getMinCost();
+
 private:
     int num_visits;               // 该节点被访问的次数
     double cost;                  // 该节点的cost
@@ -50,6 +54,7 @@ public:
  vector<ViewPoint> solveMCST();
  void getUncoveredPatch(vector<int>& uncovered_patch,
                         const vector<ViewPoint>& select_vp);  // 计算未覆盖的表面片
+ vector<vector<ViewPoint>> getTravelCostGraph();
 
 private:
     // 可按需求修改参数
